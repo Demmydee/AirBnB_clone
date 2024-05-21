4d452d83f2b5f24a7134c879887a72d62070bc69
Olaoye Ademola <demmydd44@gmail.com>
2024-05-21 10:52:50 -0700

    last correction

--- tests/test_models/models/amenity.py
+++ tests/test_models/models/amenity.py
@@ -8,6 +8,6 @@ from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
     """Amenity class that inherits from BaseModel and Base"""
-    __tabName__ = "amenities"
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary=place_amenity)
